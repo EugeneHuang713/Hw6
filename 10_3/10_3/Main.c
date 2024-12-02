f4b24ef04e3c63bcfa3bf2c947ce4e39b12a839e
Your Name <you@example.com>
2024-12-02 17:40:34 +0800

    first commit

---
+++ 10_3/10_3/Main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* card structure definition */
+struct card {
+    const char* face; /* define pointer face */
+    const char* suit; /* define pointer suit */
+}; /* end structure card */
+
+typedef struct card Card; /* new type name for struct card */
+
+/* prototypes */
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]);
+void shuffle(Card* const wDeck);
+void deal(const Card* const wDeck);
+
+int main(void)
+{
+    Card deck[52]; /* define array of Cards */
+    const char* face[] = { "Ace", "Deuce", "Three", "Four", "Five",
+        "Six", "Seven", "Eight", "Nine", "Ten",
+        "Jack", "Queen", "King" };
+    const char* suit[] = { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+    srand(time(NULL)); /* randomize */
+
+    fillDeck(deck, face, suit); /* load the deck with Cards */
+    shuffle(deck); /* put Cards in random order */
+    deal(deck); /* deal all 52 Cards */
+    return 0; /* indicates successful termination */
+}
+
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]) {
+    int i; /* counter */
+
+    for (i = 0; i <= 51; i++) {
+        wDeck[i].face = wFace[i % 13];
+        wDeck[i].suit = wSuit[i / 13];
+    }
+}
+
+void shuffle(Card* const wDeck)
+{
+    int i, j;
+    Card temp;
+
+    for (i = 0; i <= 51; i++)
+    {
+        j = rand() % 52;
+        temp = wDeck[i];
+        wDeck[i] = wDeck[j];
+        wDeck[j] = temp;
+    }
+}
+
+void deal(const Card* const wDeck)
+{
+    int i;
+
+    for (i = 0; i <= 51; i++)
+    {
+        printf("%5s of %-8s%8s", wDeck[i].face, wDeck[i].suit, (i + 1) % 4 ? "" : "\n");
+    }
+}
